0b55cc94cf7ec6fc54dc0e3b11f12c8b5fa8213d
dermotbg <dermotbateman@gmail.com>
2024-03-22 14:34:41 +0200

    done: 12.22

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -36,11 +36,6 @@ app.use('/api/avatars', avatarRouter)
 
 app.use(express.static('dist'))
 
-// uncomment for prod 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-//  })
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
